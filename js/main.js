4f6ec2efde39ca36a725418cc20f95b0728bca77
grzegorz_nowak <grzesiek6493@gmail.com>
2018-03-05 20:26:43 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,114 @@
+$(document).ready(function () {
+    addScrolledClass();
+    addSlider();
+    smoothScroll();
+});
+
+$(window).scroll(function () {
+    addScrolledClass();
+});
+
+function smoothScroll() {
+    var scrollLink = $('.scroll');
+
+    // smooth scrolling
+    scrollLink.click(function (e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000);
+    });
+
+    // active link switching
+    $(window).scroll(function () {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function () {
+
+            var sectionOffset = $(this.hash).offset().top - 20;
+
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            };
+        });
+    });
+};
+
+
+//hamburger icon animation
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+//addScrolledClass function
+function addScrolledClass() {
+    var navHeight = $('#navbar').outerHeight();
+    var actualPos = $(window).scrollTop();
+    if (actualPos >= navHeight) {
+        $('#navbar').addClass('scrolled');
+    } else {
+        $('#navbar').removeClass('scrolled');
+    }
+}
+
+//slider function
+
+function addSlider() {
+    $('.slider').bxSlider();
+}
+
+
+
+//geolocation google maps api with marker
+function initMap() {
+    var map, infoWindow;
+    map = new google.maps.Map(document.getElementById('map'), {
+
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 11,
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                //variable add marker 
+                var marker = new google.maps.Marker({
+                    position: pos,
+                    map: map,
+                });
+                map.setCenter(pos);
+
+
+            },
+            function () {
+                handleLocationError(true, infoWindow, map.getCenter());
+            });
+    } else {
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
